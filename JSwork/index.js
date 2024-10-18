455800c14c41ed6923fe1efc5c2e74ee6626d546
Ayushi1298 <ayushij2005eenu@gmail.com>
2024-10-18 08:56:19 +0530

    Updated

---
+++ JSwork/index.js
@@ -0,0 +1,16 @@
+console.log("hello js");
+// const a=12;
+var a=12;
+if(a>10){
+    // let b=5;
+    a=23;
+    console.log("a="+a);
+}
+console.log("hi, a="+a);
+// console.log("hi, a="+b);
+function fun(a,b){
+    console.log("Hello");
+    return a+b;
+}
+console.log(fun());
+console.log(fun(24,6));
