4f97abb059cff4c66680bf7e55a3d49e923bc75a
Ayushi1298 <ayushij2005eenu@gmail.com>
2024-11-05 08:54:32 +0530

    Updated

--- JSwork/index.js
+++ JSwork/index.js
@@ -8,9 +8,70 @@ if(a>10){
 }
 console.log("hi, a="+a);
 // console.log("hi, a="+b);
-function fun(a,b){
+function fun(a,b,c=10){
     console.log("Hello");
-    return a+b;
+    return a+b+c;
 }
 console.log(fun());
 console.log(fun(24,6));
+
+/*
+function cCompiler(){
+    return "C compiler";
+}
+function javaCompiler(){
+    return "Java Compiler";
+}
+function selectLanguage(clbk){
+    return "You have selected "+clbk();
+}
+const result=selectLanguage(javaCompiler);
+console.log(result)
+*/
+
+/*
+function selectLanguage(language){
+    let data ;
+    if (language =="C"){
+        function cCompiler(){
+            return "C compiler";
+        }
+        data=cCompiler();
+    }
+    if(language =="java"){
+        function javaCompiler(){
+            return "Java Compiler";
+        }
+        data=javaCompiler();
+    }
+    return data;
+}
+const result=selectLanguage();
+console.log(result)
+*/
+
+//dom
+/*
+console.dir(document);
+const div=document.getElementsByClassName("parent");
+console.log(div);
+div[0].innerHTML="<h2 style=color:red> Abes Engineering College</h2>";
+const h1=document.createElement("h1");
+h1.innerText="It was created";
+h1.style.backgoundColor='cyan';
+console.log(h1);
+div[0].appendChild(h1);
+const img=document.createElement('img');
+img.src='../Images/place.jpg';
+
+img.setAttribute("height","200");
+console.log(img);
+div[0].appendChild(img);
+*/
+
+const disp=document.getElementById("disp");
+console.dir(disp);
+function getData(){
+    console.log("Hello");
+    disp.innerHTML="<h2>Hello You have called> </h2>"
+}
